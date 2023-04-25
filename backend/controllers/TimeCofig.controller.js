f4891a2f98057100eae0763c13488465fb886251
ngochuyit <swdev04@mail.com>
2023-04-25 14:53:20 +0700

    oke

--- backend/controllers/TimeCofig.controller.js
+++ backend/controllers/TimeCofig.controller.js
@@ -18,8 +18,8 @@ const timeConfig = {
   createTimeCf: async (req, res) => {
     try {
       const newTime = await new Time({
-        time_in: req.body.time_in,
-        time_out: req.body.time_out,
+        time_morning: req.body.time_morning,
+        time_afternoon: req.body.time_afternoon,
       });
       const time = await newTime.save();
       res.status(200).json(time);
@@ -45,53 +45,3 @@ const timeConfig = {
 
 module.exports = timeConfig;
 
-// exports.getAllTimes = async (req, res) => {
-//   try {
-//     const times = await Time.find();
-//     res.status(200).json({ message: "Time successfully", times });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: "Server error" });
-//   }
-// };
-
-// exports.getTime = async (req, res) => {
-//   const { id } = req.params;
-//   try {
-//     const time = await Time.findById(id);
-
-//     if (!time) {
-//       return res.status(404).json({ message: "Time not found" });
-//     }
-//     res.status(200).json(time);
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ message: "Server Error" });
-//   }
-// };
-
-// exports.createTime = async (req, res) => {
-//   const { time_in, time_out } = req.body;
-//   try {
-//     const time = new Time({ time_in, time_out });
-//     await time.save();
-//     res.status(200).json({ message: "Time add successfully" });
-//   } catch (error) {
-//     res.status(500).json({ message: "Server Error" });
-//   }
-// };
-
-// exports.deleteTime = async (req, res) => {
-//   const { id } = req.params;
-//   try {
-//     const time = await Time.findByIdAndDelete(id);
-//     console.log(time);
-//     if (!time) {
-//       return res.status(404).json({ message: "Resource not found" });
-//     }
-//     await time.remove();
-//     res.status(200).json({ message: "Time deleted successfully" });
-//   } catch (error) {
-//     res.status(500).json({ message: "Server Error" });
-//   }
-// };
--- backend/models/TimeConfig.js
+++ backend/models/TimeConfig.js
@@ -2,12 +2,14 @@ const mongoose = require("mongoose");
 const moment = require("moment");
 
 const TimeSchema = new mongoose.Schema({
-  time_in: {
+  time_morning: {
     type: String,
   },
-  time_out: {
+
+  time_afternoon: {
     type: String,
   },
+ 
   created_at: {
     type: Date,
     default: Date.now,
