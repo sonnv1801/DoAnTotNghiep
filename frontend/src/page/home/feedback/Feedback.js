58f0e76ce687e6ff79efecc0fcc836eefb1a7753
sonnv1801 <70565643+sonnv1801@users.noreply.github.com>
2023-05-15 12:35:53 +0700

    #15 - fix bug

--- frontend/src/page/home/feedback/Feedback.js
+++ frontend/src/page/home/feedback/Feedback.js
@@ -1,52 +1,89 @@
-import React , { useState } from 'react';
-import './style.css'
-import emailjs from '@emailjs/browser';
-
-const Result = () => {
-  return(
-      <p>Your message has been successfully sent. I will contact you soon</p>
-  )
-}
+import React, { useState } from "react";
+import "./style.css";
+import emailjs from "@emailjs/browser";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import feedback from "../../../assets/feedback.png";
 
 export const FeedBack = () => {
-  const [result, showResult] = useState(false);
-
   const sendEmail = (e) => {
     e.preventDefault();
 
     emailjs
-        .sendForm('service_3fg4a3q', 'template_uysiv2m', e.target, 'cwYhvmQeBzVTrDpYe')
-        .then((result) => {
+      .sendForm(
+        "service_3fg4a3q",
+        "template_uysiv2m",
+        e.target,
+        "cwYhvmQeBzVTrDpYe"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+          toast.success("Đã nhận phản hồi. Chúng tôi sẽ phản hồi bạn sớm!", {
+            position: toast.POSITION.TOP_RIGHT,
+          });
+        },
+        (error) => {
           console.log(error.text);
-      });
-      e.target.reset();
-      showResult(true);
+          toast.error("Lỗi mất rồi!", {
+            position: toast.POSITION.TOP_RIGHT,
+          });
+        }
+      );
+    e.target.reset();
   };
   return (
-   <div className="contact-section" >
-    <div className="contact-info">
-        <div><i class='fas fa-map-marker-alt'></i>Address, City, Country</div>
-        <div><i class='fas fa-envelope'></i>contact@gmail.com</div>
-        <div><i class='fas fa-phone'></i>+98 0254 554 223</div>
-        <div><i class='fas fa-clock'></i>Mon - Fri 8:00 AM to 5:00 PM</div>
-
+    <div className="main-contact">
+      <ToastContainer />
+      <div className="contact-section">
+        <div className="contact-info">
+          {/* <div>
+          <i class="fas fa-map-marker-alt"></i>33 Xô Viết Nghệ Tĩnh, Hải Châu,
+          Đà Nẵng
+        </div>
+        <div>
+          <i class="fas fa-envelope"></i>i-workcompany@gmail.com
+        </div>
+        <div>
+          <i class="fas fa-phone"></i>+84 4742 844 40
+        </div>
+        <div>
+          <i class="fas fa-clock"></i>08:00 AM - 18:00 PM
+        </div> */}
+          <img src={feedback} alt="ffff" />
+        </div>
+        <div className="contact-form">
+          <h2>Phản hồi với chúng tôi</h2>
+          <form
+            className="contact"
+            action=""
+            onSubmit={sendEmail}
+            method="post"
+          >
+            <input
+              type="text"
+              name="fullName"
+              class="text-box"
+              placeholder="Your Name"
+              required
+            />
+            <input
+              type="email"
+              name="email"
+              class="text-box"
+              placeholder="Your Email"
+              required
+            />
+            <textarea
+              name="message"
+              rows="5"
+              placeholder="Your Message"
+              required
+            ></textarea>
+            <input type="submit" name="submit" class="send-btn" value="Send" />
+          </form>
+        </div>
+      </div>
     </div>
-    <div className='contact-form' >
-        <h2>Phản hồi với chúng tôi</h2>
-        <form className='contact' action='' onSubmit={sendEmail} method='post'>
-            <input type="text" name='fullName' class='text-box' placeholder='Your Name' required/>
-            <input type="email" name='email' class='text-box' placeholder='Your Email' required/>
-            <textarea name='message' rows='5' placeholder='Your Message' required></textarea>
-            <input type="submit" name='submit' class='send-btn' value="Send"/>
-            <div className='row'>
-                {result ? <Result/> : null}
-            </div>
-
-        </form>
-    </div>
-   </div>
-
-  )
-}
+  );
+};
